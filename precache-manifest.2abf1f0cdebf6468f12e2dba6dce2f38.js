ee13182ffc47535a8c9ca26535917759fed3aefb
Collatty <hakoncb@gmail.com>
2019-09-27 13:47:03 +0200

    Updates

---
+++ precache-manifest.2abf1f0cdebf6468f12e2dba6dce2f38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "599fc67c9e39557a1bc4039be579de80",
+    "url": "/podderud/index.html"
+  },
+  {
+    "revision": "3efad206a8a1d758ff21",
+    "url": "/podderud/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "2c34457915e44358a33c",
+    "url": "/podderud/static/css/main.c66b1d5b.chunk.css"
+  },
+  {
+    "revision": "3efad206a8a1d758ff21",
+    "url": "/podderud/static/js/2.29c5679d.chunk.js"
+  },
+  {
+    "revision": "2c34457915e44358a33c",
+    "url": "/podderud/static/js/main.54b6a06c.chunk.js"
+  },
+  {
+    "revision": "e93290df0518e0610007",
+    "url": "/podderud/static/js/runtime-main.7c7865b0.js"
+  }
+]);
